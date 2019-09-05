4888167a13301e3fb459331b4546e6be368ff837
yyj <179063828@qq.com>
2019-09-05 11:10:02 +0800

    use laravel-mix build tailwindcss

---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.postCss('src/res/app.css', 'src/main/resources/static/app.css', [
+    require('tailwindcss'),
+])
