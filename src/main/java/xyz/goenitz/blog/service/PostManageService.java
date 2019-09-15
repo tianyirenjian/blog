b7d4e56289fbb9e7b57fb845ee5fb36e1c1181d6
tianyirenjian <179063828@qq.com>
2019-09-15 23:26:35 +0800

    get all tags

--- src/main/java/xyz/goenitz/blog/service/PostManageService.java
+++ src/main/java/xyz/goenitz/blog/service/PostManageService.java
@@ -4,16 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.UnwindOperation;
 import org.springframework.stereotype.Service;
 import xyz.goenitz.blog.model.Post;
 import xyz.goenitz.blog.repository.PostRepository;
 
 import java.time.Instant;
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.unwind;
 
 @Service
 public class PostManageService {
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public Page<Post> getPostList(int page, String keyword, String tag) {
         int size = 10;
@@ -46,5 +58,14 @@ public class PostManageService {
 
     public void deletePost(String id) {
         postRepository.deleteById(id);
+    }
+
+    public List<Object> getAllTags() {
+        UnwindOperation unwindOperation = unwind("tags");
+        GroupOperation groupOperation = group("tags").count().as("count");
+        Aggregation aggregation = Aggregation.newAggregation(unwindOperation, groupOperation);
+
+        AggregationResults<Object> results = mongoTemplate.aggregate(aggregation, Post.class, Object.class);
+        return results.getMappedResults();
     }
 }
