a59ea17b325cf9784e9f6d4a9f7ae76c38711c56
tianyirenjian <179063828@qq.com>
2019-10-01 13:19:48 +0800

    login success set lastLogin

--- src/main/java/xyz/goenitz/blog/WebSecurityConfig.java
+++ src/main/java/xyz/goenitz/blog/WebSecurityConfig.java
@@ -8,8 +8,20 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import xyz.goenitz.blog.model.User;
+import xyz.goenitz.blog.repository.UserRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Instant;
 
 @Configuration
 @EnableWebSecurity
@@ -29,8 +41,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
+                .successHandler(myAuthenticationSuccessHandler())
                 .and()
                 .logout().permitAll();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+        return new MySavedRequestAwareAuthenticationSuccessHandler();
     }
 
     @Bean
@@ -41,5 +59,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
         builder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+}
+
+class MySavedRequestAwareAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Object principal = authentication.getPrincipal();
+        String username = ((UserDetails)principal).getUsername();
+        User user = userRepository.findByUsername(username);
+        user.setLastLogin(Instant.now());
+        userRepository.save(user);
+        super.onAuthenticationSuccess(request, response, authentication);
     }
 }
